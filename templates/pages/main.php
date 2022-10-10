bdbedb57e6fa673bf07a5d92b67d4b65f0eb7f68
Denverss <102513124+Denverss@users.noreply.github.com>
2022-10-10 22:28:05 +0300

    Update main.php

--- templates/pages/main.php
+++ templates/pages/main.php
@@ -1,94 +1,68 @@
-<?php
-$Content = include_template('layout.php', ['content' => '<div class="content">
-<section class="content__side">
-    <h2 class="content__side-heading">Проекты</h2>
+<div class="content">
+    <section class="content__side">
+        <h2 class="content__side-heading">Проекты</h2>
 
-    <nav class="main-navigation">
-        <ul class="main-navigation__list">
-            <?php foreach ($categories as $category):?>
-                <li class="main-navigation__list-item">
-                    <a class="main-navigation__list-item-link" href="#"><?= $category ?></a>
-                    <span class="main-navigation__list-item-count"><?= count_category($posts,$category) ?></span>
-                </li>
-            <?php endforeach;?>
-        </ul>
-    </nav>
-
-    <a class="button button--transparent button--plus content__side-button"
-       href="form-project.html" target="project_add">Добавить проект</a>
-</section>
-
-<main class="content__main">
-    <h2 class="content__main-heading">Список задач</h2>
-
-    <form class="search-form" action="index.php" method="post" autocomplete="off">
-        <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
-
-        <input class="search-form__submit" type="submit" name="" value="Искать">
-    </form>
-
-    <div class="tasks-controls">
-        <nav class="tasks-switch">
-            <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
-            <a href="/" class="tasks-switch__item">Повестка дня</a>
-            <a href="/" class="tasks-switch__item">Завтра</a>
-            <a href="/" class="tasks-switch__item">Просроченные</a>
+        <nav class="main-navigation">
+            <ul class="main-navigation__list">
+                <?php foreach ($categories as $category):?>
+                    <li class="main-navigation__list-item">
+                        <a class="main-navigation__list-item-link" href="#"><?= $category ?></a>
+                        <span class="main-navigation__list-item-count"><?= count_category($posts,$category) ?></span>
+                    </li>
+                <?php endforeach;?>
+            </ul>
         </nav>
 
-        <label class="checkbox">
-            <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-            <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if($show_complete_tasks == 1):?> checked   <?php endif; ?>>
-            <span class="checkbox__text">Показывать выполненные</span>
-        </label>
-    </div>
+        <a class="button button--transparent button--plus content__side-button"
+            href="pages/form-project.html" target="project_add">Добавить проект</a>
+     </section>
 
-    <table class="tasks">
-        <?php foreach ($posts as $post): ?>
-            <tr class="tasks__item task">
-                <td class="task__select">
-                    <label class="checkbox task__checkbox">
-                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-                        <span class="checkbox__text"><?= $post["title"]?></span>
-                    </label>
-                </td>
+    <main class="content__main">
+        <h2 class="content__main-heading">Список задач</h2>
 
-                <td class="task__select">
+        <form class="search-form" action="index.php" method="post" autocomplete="off">
+            <input class="search-form__input" type="text" name="" value="" placeholder="Поиск по задачам">
 
-                    <span class="task__date"><?=$post["data"]?></span>
+            <input class="search-form__submit" type="submit" name="" value="Искать">
+        </form>
 
-                </td>
+        <div class="tasks-controls">
+            <nav class="tasks-switch">
+                <a href="/" class="tasks-switch__item tasks-switch__item--active">Все задачи</a>
+                <a href="/" class="tasks-switch__item">Повестка дня</a>
+                <a href="/" class="tasks-switch__item">Завтра</a>
+                <a href="/" class="tasks-switch__item">Просроченные</a>
+            </nav>
 
-                <td class="task__select">
+            <label class="checkbox">
+                <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
+                <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if($show_complete_tasks == 1):?> checked   <?php endif; ?>>
+                <span class="checkbox__text">Показывать выполненные</span>
+            </label>
+         </div>
 
-                    <span class="task__categories"><?=$post["categories"]?></span>
-                </td>
-                <td class="task--completed">
-                    <?php if($show_complete_tasks===0):?>
-                        <span class="task--completed"><?=$post["is_done"]?></span>
-                    <?php endif;?>
-                </td>
+        <table class="tasks">
+            <?php foreach ($posts as $post): ?>
+                <?php if ($show_complete_tasks === 0 && $post["is_done"]): ?>
+                     <?php continue; ?>
+                 <?php else: ?>
+                     <tr class="tasks__item task <?= ($post["is_done"]) ? 'task--completed' : ''?>">
+                            <td class="task__select">
+                             <label class="checkbox task__checkbox">
+                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                                <span class="checkbox__text"><?= $post["title"]?></span>
+                            </label>
+                         </td>
+                        <td class="task__file">
+                            <a class="download-link" href="#">Home.psd</a>
+                        </td>
 
-            </tr>
-            <?php if($show_complete_tasks == 1): ?>
-                <tr class="tasks__item task task--completed ">
+                        <td class="task__date"><?= $post["data"] ?></td>
 
-                    <td class="task__select">
-
-                        <label class="checkbox task__checkbox">
-                            <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-                        </label>
-                    </td>
-                    <td class="task__date"10.10.2019</td>
-                    <td class="task__controls">
-
-                    </td>
-
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-        <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-    </table>
-</main>
-</div>', 'categories' => $categories, 'posts' => $posts , 'show_complete_tasks' => $show_complete_tasks]);
-print $Content;
+                    </tr>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+        </table>
+    </main>
+ </div>
