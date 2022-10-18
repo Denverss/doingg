cd03c9c25ba857c0af7112bb742292bab1a7c3b2
test <gitkab2022@mail.ru>
2022-10-18 17:36:41 +0300

    CLASS_FIX_1.1

--- queries.sql
+++ queries.sql
@@ -6,49 +6,53 @@ CREATE TABLE users (
                      id	    BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      nickname	    VARCHAR(255) NOT NULL,
                      email	VARCHAR(255) NOT NULL UNIQUE,
-                     password VARCHAR(255) NOT NULL
+                     password VARCHAR(255) NOT NULL,
+                     creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 
 CREATE TABLE projects (
                           id	    BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                          title	VARCHAR(255) NOT NULL UNIQUE
-
+                          title	VARCHAR(255) NOT NULL UNIQUE,
+                          user_id BIGINT NOT NULL,
+                          FOREIGN KEY(user_id) REFERENCES users(id)
 
 );
 
 CREATE TABLE tasks (
                      id			BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      user_id		BIGINT NOT NULL,
-                     date_create DATETIME NOT NULL,
                      project_id	BIGINT NOT NULL,
+                     date_create DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                      title		VARCHAR(255) NOT NULL,
-                     file  VARCHAR(255) NOT NULL,
-                     is_done VARCHAR(1) ,
+                     file  VARCHAR(255) NULL,
+                     is_done TINYINT NOT NULL DEFAULT 0,
+                     date_end DATETIME NULL,
                      FOREIGN KEY(user_id) REFERENCES users(id),
-                      FOREIGN KEY(project_id) REFERENCES  projects(id)
+                     FOREIGN KEY(project_id) REFERENCES  projects(id)
 
 );
 
+INSERT INTO users(nickname, email, password)
+VALUES ('Andrew', 'my@mail.ru', '$2y$10$azTPD8NrWOdSm5GIrE5J/OE9Usc5JB4rq4CpR4xYv7Yc2Yb01ODXW'),
+       ('Danil', 'denbach@mail.ru','123bach'),
+       ('Oli','anonymus@mail.ru','olyaSyperstar');
 
-INSERT INTO projects(title)
-VALUES ('Входящие'),
-       ('Учеба'),
-       ('Работа'),
-       ('Домашние дела'),
-       ('Авто');
-INSERT INTO users(id, nickname, email, password)
-VALUES (1,'Andrew', 'my@mail.ru', '$2y$10$azTPD8NrWOdSm5GIrE5J/OE9Usc5JB4rq4CpR4xYv7Yc2Yb01ODXW'),
-(2,'Danil', 'denbach@mail.ru','123bach'),
-(3,'Oli','anonymus@mail.ru','olyaSyperstar');
+INSERT INTO projects(title, user_id)
+VALUES ('Входящие',1),
+       ('Учеба',1),
+       ('Работа',1),
+       ('Домашние дела',1),
+       ('Авто',1);
 
-INSERT INTO tasks(user_id, title,date_create,project_id,is_done, file)
-VALUES (1,'Собеседование в IT компании', '01.12.2019',3,'0','Link'),
-       (2,'Выполнить тестовое задание', '25.12.2019',3,'0','File'),
-       (3,'Сделать задание первого раздела', '21.12.2019',2,'1','File'),
-       (1,'Встреча с другом', '22.12.2019',1,'0','File'),
-       (2,'Купить корм для кота', '20.10.2022',4,'0','File'),
-       (3,'Заказать пиццу', '15.10.2022',4,'0','Link');
+
+INSERT INTO tasks(user_id, title,date_end,project_id,is_done, file)
+VALUES (1,'Собеседование в IT компании', '01.12.2019',3,0,'Link'),
+       (2,'Выполнить тестовое задание', '25.12.2019',3,0,'File'),
+       (3,'Сделать задание первого раздела', '21.12.2019',2,1,'File'),
+       (1,'Встреча с другом', '22.12.2019',1,0,'File'),
+       (2,'Купить корм для кота', '20.10.2022',4,0,'File'),
+       (3,'Заказать пиццу', '15.10.2022',4,0,'Link');
 
 SELECT id,
       nickname,
